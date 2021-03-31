d587f38d89511a8099a17362d5db0fa4cf33c576
gviloria001 <77612109+gviloria001@users.noreply.github.com>
2021-03-31 00:03:35 -0700

    Add files via upload

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,16 +1,19 @@
-#include <iostream>;
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 
 int evaluation(char board[3][3]);
-int miniMax(char board[3][3], bool isMax);
+int miniMax(char board[3][3], bool isMax, int depth);
 bool movesLeft(char board[3][3]);
+void displayBoard(char board[3][3]);
 bool gameOver(char board[3][3]) {
-	if (movesLeft(board) == false) {
+	if (!movesLeft(board)) {
 		return true;
 	}
-	return false;
+	else
+		return false;
 }
 class Player {
 private:
@@ -22,10 +25,22 @@ public:
 		cin >> x;
 		cout << "Input y:" << endl;
 		cin >> y;
+		while (!isValid(board, x, y)) {
+			cout << "Input valid x:" << endl;
+			cin >> x;
+			cout << "Input valid y:" << endl;
+			cin >> y;
+		}
 		makePlayerMove(board, x, y);
 	}
 	void makePlayerMove(char board[3][3], int x, int y) {
 		board[y][x] = 'O';
+	}
+	bool isValid(char board[3][3], int x, int y) {
+		if (board[y][x] == 'X' || board[y][x] == 'O')
+			return false;
+		else
+			return true;
 	}
 };
 class Move {
@@ -35,29 +50,21 @@ private:
 	int bestCol = -1;
 public:
 	void bestMove(char board[3][3]) {
-		/*int depth = 0;
-		depth++;
-		cout << "Depth: " << depth << endl;*/
+		int bestVal = -100;
 		for (int row = 0; row < 3; row++) {
 			for (int col = 0; col < 3; col++) {
 				if (board[row][col] == ' ') {
-					//cout << "Line 42: " << row << " " << col << " " << endl;
 					board[row][col] = 'X';
-					int compareVal = miniMax(board, false);
-					//cout << "Line 44: " << compareVal << endl;
+					int compareVal = miniMax(board, false, 0);
 					board[row][col] = ' ';
-					if (compareVal > bestVal) {
-						//cout << "in Line 46" << endl;
+					if (bestVal < compareVal) {
 						bestRow = row;
 						bestCol = col;
-						//cout << bestRow << "," << bestCol << endl;
 						bestVal = compareVal;
 					}
 				}
 			}
 		}
-		bestVal = 0;
-		//cout << bestRow << endl << bestCol << endl;
 	}
 	int getBestRow() {
 		return bestRow;
@@ -79,67 +86,141 @@ bool movesLeft(char board[3][3]) {
 	}
 	return false;
 }
-int miniMax(char board[3][3], bool isMax) {
+int miniMax(char board[3][3], bool isMax, int depth) {
+
 	int score = evaluation(board);
-	//if (movesLeft(board) == false)
-	//	return 0;
-	if (isMax) {
-		int best = -100;
+	if (movesLeft(board) == false)
+		return 0;
+	if (score >= 100)
+		return score;
+	if (score <= -100)
+		return score;
+	if (isMax == true) {
+		int best = -1000;
 		for (int row = 0; row < 3; row++) {
 			for (int col = 0; col < 3; col++) {
 				if (board[row][col] == ' ') {
 					board[row][col] = 'X';
-					best = max(best, miniMax(board, false));
+					int value = miniMax(board, !isMax, depth + 1);
+					best = std::max(best, value);
 					board[row][col] = ' ';
 				}
 			}
 		}
-		return best + score;
+		return best;
 	}
 	else {
-		int best = 100;
+		int least = 1000;
 		for (int row = 0; row < 3; row++) {
 			for (int col = 0; col < 3; col++) {
 				if (board[row][col] == ' ') {
 					board[row][col] = 'O';
-					best = min(best, miniMax(board, true));
+					int value = miniMax(board, !isMax, depth + 1);
+					least = std::min(least, value);
 					board[row][col] = ' ';
 				}
 			}
 		}
-		return best + score;
+		return least;
 	}
 }
 int evaluation(char board[3][3]) {
-	for (int rows = 0; rows < 3; rows++) {
-		if (board[rows][0] == board[rows][1] && board[rows][1] == board[rows][2]) {
-			if (board[rows][0] == 'X')
-				return +10;
-			else if (board[rows][0] == 'O')
-				return -10;
+	int score = 0;
+	if (movesLeft(board) == false)
+		return 0;
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//Count the amount of Xs and Os in the rows. +100 3X, +10 2x, +1  1x, 0 cats game, -1 1o, -10 2o, -100 3o
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	for (int row = 0; row < 3; row++) {								
+		if (board[row][0] == 'X' || board[row][1] == 'X' || board[row][2] == 'X') {
+			if (board[row][0] == board[row][1] || board[row][0] == board[row][2] || board[row][1] == board[row][2]) {
+				if (board[row][0] == board[row][1] && board[row][0] == board[row][2]) {
+					score += 100;
+				}
+				score += 10;
+			}
+			score += 1;
+		}
+		else if (board[row][0] == 'O' || board[row][1] == 'O' || board[row][2] == 'O') {
+			if (board[row][0] == board[row][1] || board[row][0] == board[row][2] || board[row][1] == board[row][2]) {
+				if (board[row][0] == board[row][1] && board[row][0] == board[row][2]) {
+					score -= 100;
+				}
+				score -= 10;
+			}
+			score -= 1;
 		}
 	}
-	for (int cols = 0; cols < 3; cols++) {
-		if (board[0][cols] == board[1][cols] && board[1][cols] == board[2][cols]) {
-			if (board[0][cols] == 'X')
-				return +10;
-			else if (board[0][cols] == 'O')
-				return -10;
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//Count the amount of Xs and Os in the cols. +100 3X, +10 2x, +1  1x, 0 cats game, -1 1o, -10 2o, -100 3o
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	for (int col = 0; col < 3; col++) {
+		if (board[0][col] == 'X' || board[1][col] == 'X' || board[2][col] == 'X') {
+			if (board[0][col] == board[1][col] || board[0][col] == board[2][col] || board[1][col] == board[2][col]) {
+				if (board[0][col] == board[1][col] && board[0][col] == board[2][col]) {
+					score += 100;
+				}
+				score += 10;
+			}
+			score += 1;
+		}
+		else if (board[0][col] == 'O' || board[1][col] == 'O' || board[2][col] == 'O') {
+			if (board[0][col] == board[1][col] || board[0][col] == board[2][col] || board[1][col] == board[2][col]) {
+				if (board[0][col] == board[1][col] && board[0][col] == board[2][col]) {
+					score -= 100;
+				}
+				score -= 10;
+			}
+			score -= 1;
 		}
 	}
-	if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-		if (board[0][0] == 'X')
-			return +10;
-		else if (board[0][0] == 'O')
-			return -10;
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//Count the amount of Xs and Os in the diagonals. +100 3X, +10 2x, +1  1x, 0 cats game, -1 1o, -10 2o, -100 3o
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//Top right to bottom left
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	if (board[0][2] == 'X' || board[1][1] == 'X' || board[2][0] == 'X') {
+		if (board[0][2] == board[1][1] || board[0][2] == board[2][0] || board[1][1] == board[2][0]) {
+			if (board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+				score += 100;
+			}
+			score += 10;
+		}
+		score += 1;
 	}
-	if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
-		if (board[0][2] == 'X')
-			return +10;
-		else if (board[0][2] == 'O')
-			return -10;
+	if (board[0][2] == 'O' || board[1][1] == 'O' || board[2][0] == 'O') {
+		if (board[0][2] == board[1][1] || board[0][2] == board[2][0] || board[1][1] == board[2][0]) {
+			if (board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+				score -= 100;
+			}
+			score -= 10;
+		}
+		score -= 1;
 	}
-	return 0;
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//Top left to bottom right
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	if (board[0][0] == 'X' || board[1][1] == 'X' || board[2][2] == 'X') {
+		if (board[0][0] == board[1][1] || board[0][0] == board[2][2] || board[1][1] == board[2][2]) {
+			if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+				score += 100;
+			}
+			score += 10;
+		}
+		score += 1;
+	}
+	if (board[0][0] == 'O' || board[1][1] == 'O' || board[2][2] == 'O') {
+		if (board[0][0] == board[1][1] || board[0][0] == board[2][2] || board[1][1] == board[2][2]) {
+			if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+				score -= 100;
+			}
+			score -= 10;
+		}
+		score -= 1;
+	}
+	return score;
 }
 void displayBoard(char board[3][3]) {
 	cout << "=====================" << endl;
@@ -149,6 +230,26 @@ void displayBoard(char board[3][3]) {
 	cout << " Y  1 " << board[1][0] << "|" << board[1][1] << "|" << board[1][2] << endl;
 	cout << "axis  " << "-+-+-" << endl;
 	cout << "    2 " << board[2][0] << "|" << board[2][1] << "|" << board[2][2] << endl << endl;
+}
+bool isWinner(char board[3][3], char symbol) {
+	for (int i = 0; i < 3; i++) {	// i	check each row
+		if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol)// 0
+			return true;
+	}
+	// for each column
+	// i  0  1  2
+	for (int i = 0; i < 3; i++) {
+		if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol)
+			return true;
+
+	}
+	//  or diagonal left to right down
+	if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol)
+		return true;
+	if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol)
+		return true;
+	else
+		return false;
 }
 int main() {
 	char board[3][3] = {
@@ -160,13 +261,29 @@ int main() {
 	Player player;
 	int x = 0;
 	int y = 0;
-	while (!gameOver(board)) {
+	while (!movesLeft(board) || !isWinner(board, 'X' || !isWinner(board, 'O'))) {
 		BestMove.bestMove(board);
-		//cout << BestMove.getBestRow() << " , " << BestMove.getBestCol() << endl;
 		board[BestMove.getBestRow()][BestMove.getBestCol()] = 'X';
 		displayBoard(board);
+		if (!movesLeft(board) || isWinner(board, 'X')){
+			break;
+		}
 		player.playerMove(board);
 		displayBoard(board);
+		if (!movesLeft(board) || isWinner(board, 'O')) {
+			break;
+		}
+	}
+	if (!movesLeft(board)) {
+		cout << "================\n" << "= Cats Game!!! =\n" << "================\n";
+	}
+	else {
+		if (isWinner(board, 'X')) {
+			cout << "================\n" << "=   X Wins!!!  =\n" << "================\n";
+		}
+		else {
+			cout << "================\n" << "=   O Wins!!!  =\n" << "================\n";
+		}
 	}
 	return 0;
 }
